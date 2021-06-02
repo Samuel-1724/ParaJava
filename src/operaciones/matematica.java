67df157a3b5e3ed103db90e9397cd5499aeb1cff
Samuel Aldaz <SAmd2nte@hotmail.com>
2021-06-02 10:42:30 -0500

    v1 Primer vez utilizando git en java

---
+++ src/operaciones/matematica.java
@@ -0,0 +1,12 @@
+package operaciones;
+
+public class matematica {
+
+    int a;
+    int b;
+
+    public void sumar() {
+        int resultado = a + b;
+        System.out.println("El RESULTADO ES " + resultado);
+    }
+}
